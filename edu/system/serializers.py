1826500ad139abe01f2962f9118032d66ea53449
pythoncaster <147022375+pythoncaster@users.noreply.github.com>
2023-10-05 15:21:34 +0300

    percent

--- edu/system/serializers.py
+++ edu/system/serializers.py
@@ -16,10 +16,18 @@ class ProductSerializer(serializers.ModelSerializer):
 
 class ProductWithAccessCountSerializer(serializers.ModelSerializer):
     access_count = serializers.SerializerMethodField()
+    percent = serializers.SerializerMethodField()
 
     class Meta:
         model = Product
-        fields = ('id', 'title', 'access_count')
+        fields = ('id', 'title', 'access_count', 'percent')
 
     def get_access_count(self, obj):
         return obj.productaccess_set.count()
+
+    def get_percent(self, obj):
+        users_count = User.objects.count()
+        if users_count > 0:
+            return (obj.productaccess_set.count() / users_count) * 100
+        else:
+            return 0
